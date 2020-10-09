146663ee438d0a6cf9d1626d6547130bf0a42457
Mahesh Kamath <maheshdk@temenos.com>
2020-10-09 20:41:41 +0530

    some more modifications

--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
@@ -1,8 +1,11 @@
 package io.javabrains.movieinfoservice.resources;
 
 import io.javabrains.movieinfoservice.models.Movie;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +13,10 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("/")
 public class MovieResource {
+    private static Logger log = LoggerFactory.getLogger(MovieResource.class);
+
 
     @Value("${server.port}")
     private int serverPort;
@@ -26,8 +31,15 @@ public class MovieResource {
         }
     }
 
-    @RequestMapping("/{movieId}")
-    public Movie getMovieInfo(@PathVariable ("movieId") String movieId){
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @RequestMapping("/movies/{movieId}")
+    public Movie getMovieInfo(@PathVariable ("movieId") String movieId, @RequestHeader("authorization") String authzToken){
+
+        log.info("authorization :"+authzToken);
         return new Movie(movieId, movieId+" --- being serving from "+ip+" port"+serverPort);
     }
 }
--- ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingsResource.java
@@ -1,32 +1,81 @@
 package io.javabrains.ratingsdataservice.resources;
 
+import io.javabrains.ratingsdataservice.models.Movie;
 import io.javabrains.ratingsdataservice.models.Rating;
 import io.javabrains.ratingsdataservice.models.UserRating;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/ratingsdata")
+@RequestMapping("/")
 public class RatingsResource {
+    private static Logger log = LoggerFactory.getLogger(RatingsResource.class);
 
-    @RequestMapping("/{movieId}")
+
+    @Bean
+    //@LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate = getRestTemplate();
+
+    @Bean
+//    @LoadBalanced
+    public WebClient.Builder getWebClient(){
+        return WebClient.builder();
+    }
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @RequestMapping("/ratingsdata/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
+
         return new Rating(movieId, 4);
     }
 
-    @RequestMapping("users/{userId}")
-    public UserRating getUserRating(@PathVariable("userId") String userId){
+    @RequestMapping("/ratingsdata/users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId,@RequestHeader("authorization") String authzToken) {
+
+        log.info("authorization :"+authzToken);
+
+      /*  Movie movie = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8089/movies/123")
+                .header("Authorization",authzToken)
+                .retrieve()
+                .bodyToMono(Movie.class)
+                .block();
+
+
+        System.out.println(">>>> movie - "+movie.getName());*/
+
         List<Rating> ratings = Arrays.asList(
                 new Rating("1000", 4),
                 new Rating("5678", 3));
 
         UserRating userRating = new UserRating();
         userRating.setUserRating(ratings);
-
+log.info("returning user rating");
         return userRating;
     }
 
